1b99efdab96c00e73e15cc73f3a7d346c55e0813
zeqingwang <61963896+zeqingwang@users.noreply.github.com>
2023-09-10 00:31:59 -0700

    Update the people section

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,10 +4,10 @@ export const Header = (props) => {
   return (
     <header id='header'>
       <div className='intro'>
-        <ParticlesBg type="circle" bg={{zIndex: 0, position:"absolute", top:0}} />
+        <ParticlesBg type="circle" bg={{ zIndex: 0, position: "absolute", top: 0 }} />
         <div className='overlay'>
           <div className='container'>
-          
+
             <div className='row'>
               <div className='col-md-8 col-md-offset-2 intro-text'>
                 <h1>
